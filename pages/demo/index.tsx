986f580b6750671d99943f2285f470183f461503
Abdul Rehman Nasir Tobaria <116266103+abdulrehmann231@users.noreply.github.com>
2025-06-29 00:14:58 +0000

    demo page

---
+++ pages/demo/index.tsx
@@ -0,0 +1,14 @@
+import type { NextPage } from "next";
+//import Head from "next/head";
+//import Image from "next/image";
+import styles from "../styles/Home.module.css";
+
+const demo: NextPage = () => {
+  return (
+    <div className={styles.container}>
+      <p>testing demo</p>
+    </div>
+  );
+};
+
+export default demo ;
